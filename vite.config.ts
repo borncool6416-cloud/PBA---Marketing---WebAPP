5cb03f56f58e8e5c8cf6f25f32ae311622e3ec8a
borncool6416-cloud <borncool6416@gmail.com>
2026-03-14 17:42:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,5 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0',
     allowedHosts: 'all'
-  },
-  build: {
-    rollupOptions: {
-      external: ['hono/jsx/jsx-runtime', 'hono/jsx/jsx-dev-runtime'],
-    }
   }
 })
